ebff2a12564e58598c2bc816d3608505c8ba55d6
Sam Abdelfattah <samfatth@gmail.com>
2017-08-16 14:44:40 -0600

    Updated Calc

--- calculator.rb
+++ calculator.rb
@@ -1,9 +1,27 @@
 require 'pry'
 
-puts 'Welcome to the ruby calculator!'
+puts "Welcome to the ruby calculator!"
 
-puts "What is your first number?"
+def start
+    puts "What is your first number?"
+    num1 = gets.to_f
+    puts "What is your operator?"
+    ops = gets.strip
+        if ops == ['+', '-', '/', '*']
+            puts "Calcuating..."
+        elsif num1 || ops || num2 == "quit"
+            exit
+        else 
+            puts "Sorry that operator is not supported. I can only use +, - , /, *."
+            puts "Please try again."
+            start
+        end    
+    puts "What is your second number?"
+    num2 = gets.to_f
+    puts "Here is your answer: "
+    
 
-puts "What is your operator?"
+    menu
+end
 
-puts "What is your second number?"
+start
