91585d9076f719ba6610e9ee5e6d32ab808dbf8b
Sam Abdelfattah <samfatth@gmail.com>
2017-08-17 09:16:17 -0600

    Working Calcultor

--- calculator.rb
+++ calculator.rb
@@ -3,25 +3,48 @@ require 'pry'
 puts "Welcome to the ruby calculator!"
 
 def start
+    # need to make rules for when user doesn't input a number for num1/num2
     puts "What is your first number?"
-    num1 = gets.to_f
-    puts "What is your operator?"
-    ops = gets.strip
-        if ops == ['+', '-', '/', '*']
-            puts "Calcuating..."
-        elsif num1 || ops || num2 == "quit"
-            exit
-        else 
-            puts "Sorry that operator is not supported. I can only use +, - , /, *."
-            puts "Please try again."
-            start
-        end    
-    puts "What is your second number?"
-    num2 = gets.to_f
-    puts "Here is your answer: "
-    
+        num1 = gets.to_f
+        #     if num1 = ('a'..'z')
+        #         puts "Invalid character, I can only use numbers. Please try again."
+            #else
+                puts "What is your operator?"
+                ops = gets.strip
+            #end
+            
+            
+                # if statement for the operator
+                if ['+', '-', '/', '*'].include?(ops)
+                    puts "What is your second number?"
+                    num2 = gets.to_f
+                        # if statement for results
+                        if ops == "+"
+                                result = num1 + num2
+                                puts "Your answer is: #{result}"
+                            elsif ops == '-'
+                                result = num1 - num2
+                                puts "Your answer is: #{result}"
+                            elsif ops == '/'
+                                result = num1 / num2
+                                puts "Your answer is: #{result}"
+                            elsif ops == '*'
+                                result = num1 * num2
+                                puts "Your answer is: #{result}"
+                            else
+                                start
+                        end
+                    # Display the results 
+                    start
+                else # If the user doesn't use a correct operation
+                    puts "Sorry #{ops} is not supported. I can only do these operators: +, - , /, *."
+                    puts "Please try again."
+                    start
+                end
+    case
+        when num1 || ops || num2 == 'quit'
+        exit
+    end  
 
-    menu
 end
-
 start
